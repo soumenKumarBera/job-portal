0796139dd2cd6fb67107c091fb104e9f54561a45
soumen kumar bera <soumenbera4017@gmail.com>
2026-06-08 16:38:40 +0530

    find job page complite

--- src/Findjobs/Jobs.tsx
+++ src/Findjobs/Jobs.tsx
@@ -1,13 +1,20 @@
+import JobCart from "./JobCart";
 import Sort from "./Sort";
+import { jobList } from "../Data/JobsData";
 
 const Jobs = () => {
   return (
-    <div className="px-5 py-4">
+    <div className="p-5">
       <div className="flex justify-between ">
         <div className="text-2xl text-mine-shaft-200">Recommended Jobs</div>
         <div>
           <Sort />
         </div>
+      </div>
+      <div className="mt-10 gap-8 flex flex-wrap justify-center">
+        {jobList.map((job, index) => (
+          <JobCart key={index} {...job} />
+        ))}
       </div>
     </div>
   );
