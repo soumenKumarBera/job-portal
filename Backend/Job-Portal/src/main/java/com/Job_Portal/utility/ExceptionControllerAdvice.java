349256ee4c9504ef19a56ec05cc3796441d060df
soumen kumar bera <soumenbera4017@gmail.com>
2026-06-19 23:18:16 +0530

    Validation Exception handling create

--- Backend/Job-Portal/src/main/java/com/Job_Portal/utility/ExceptionControllerAdvice.java
+++ Backend/Job-Portal/src/main/java/com/Job_Portal/utility/ExceptionControllerAdvice.java
@@ -1,12 +1,17 @@
 package com.Job_Portal.utility;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.bson.codecs.jsr310.LocalDateTimeCodec;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
@@ -17,6 +22,25 @@ public class ExceptionControllerAdvice {
         ErrorInfo error = new ErrorInfo(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value(),  LocalDateTime.now() );
 
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
+    public ResponseEntity<ErrorInfo>validationExceptionHandler(Exception exception){
+
+   String  msg = "";
+
+   if (exception instanceof MethodArgumentNotValidException manvException){
+       msg = manvException.getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.joining(", "));
+
+   }else {
+       ConstraintViolationException cvException = (ConstraintViolationException)exception;
+       msg = cvException.getConstraintViolations().stream().map(ConstraintViolation::getMessage).collect(Collectors.joining(", "));
+   }
+
+        ErrorInfo error = new ErrorInfo(msg, HttpStatus.BAD_REQUEST.value(),  LocalDateTime.now() );
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+
     }
 
 
