1de06f805365259189aec2f601aa5a5edd10b2f7
soumen kumar bera <soumenbera4017@gmail.com>
2026-06-19 23:36:16 +0530

    password Validation create

--- Backend/Job-Portal/src/main/java/com/Job_Portal/dto/UserDto.java
+++ Backend/Job-Portal/src/main/java/com/Job_Portal/dto/UserDto.java
@@ -1,6 +1,8 @@
 package com.Job_Portal.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,9 +21,14 @@ public class UserDto {
     private String name;
 
     @NotBlank(message ="Email in null or blank")
+    @Email(message = "Email is invalid")
     private String email;
 
     @NotBlank(message = "password in null or blank")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$",
+            message = "Password must contain 1 uppercase, 1 lowercase, 1 number, 1 special character and be 8-15 characters long"
+    )
     private String password;
 
 
