966771ddc4c327481fa5c93fce23e0e9c0bb3fbd
soumen kumar bera <soumenbera4017@gmail.com>
2026-07-02 17:16:44 +0530

    register and login fortent api create

---
+++ Frontend/job-portal/src/Servicess/UserServices.tsx
@@ -0,0 +1,18 @@
+import axios from "axios"
+const baseUrl = "http://localhost:8080/users/"
+
+const registerUser = async (user: any) => {
+
+  return axios.post(`${baseUrl}register`, user)
+  .then((response) => {response.data})
+  .catch((error) => {throw error})
+
+}
+
+const loginUser = async (login: any) => {
+
+  return axios.post(`${baseUrl}login`, login)
+  .then((response) => {response.data})
+  .catch((error) => {throw error})
+
+}
