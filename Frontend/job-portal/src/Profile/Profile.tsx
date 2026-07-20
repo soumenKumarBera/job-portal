0924ae0e059978eac0cefbde5522b46572ccad1f
soumen kumar bera <soumenbera4017@gmail.com>
2026-07-20 16:20:20 +0530

    profile photo edit

--- Frontend/job-portal/src/Profile/Profile.tsx
+++ Frontend/job-portal/src/Profile/Profile.tsx
@@ -3,11 +3,12 @@ import {
   IconBraces,
   IconBriefcase,
   IconDeviceFloppy,
+  IconEdit,
   IconMapPin,
   IconPencil,
   IconPlus,
 } from "@tabler/icons-react";
-import { Button } from "@mantine/core";
+import { Avatar, Button, FileInput, Overlay } from "@mantine/core";
 import { Divider, ActionIcon } from "@mantine/core";
 import ExpCard from "./ExpCard";
 import CertCard from "./CertCard";
@@ -26,6 +27,7 @@ import About from "./About";
 import Skills from "./Skills";
 import Experience from "./Experience";
 import Certificate from "./Certification";
+import { useHover } from "@mantine/hooks";
 
 const Profile = (props: any) => {
   const user = useSelector((state: any) => state.user);
@@ -73,25 +75,38 @@ const Profile = (props: any) => {
       });
   }, []);
 
+  const { hovered, ref } = useHover();
+  const handelFileChange =() => {
+
+    
+  }
+
   return (
     <div className="w-4/5 pb-10 mx-auto">
       <div className="relative">
         <img className="rounded-t-xl" src="/Profile/banner.jpg" alt="" />
-        <img
-          className="rounded-full w-48 h-48 absolute -bottom-1/3 left-3 border-8 border-mine-shaft-800"
-          src="avatar.png"
-          alt=""
-        />
+        <div ref={ref} className="absolute -bottom-1/3 left-3 flex items-center justify-center">
+          <Avatar
+            className="!w-48 !h-48  border-8
+            border-mine-shaft-700
+            rounded-full"
+            src="/Avatar.png"
+            alt=""
+          />
+          {hovered &&  <Overlay color="#000" backgroundOpacity={0.85}  className="!rounded-full"/>}
+          {hovered && <IconEdit className="absolute z-[300] !w-16 !h-16" />}
+          {hovered && <FileInput onChange={handelFileChange} className="absolute  z-[301] [&_*]:!h-full !h-full w-full [&_*]:!rounded-full" variant="transparent" accept="image/png, image/jpeg" />}
+        </div>
       </div>
       <div className="px-3 mt-25">
         <Info {...props} />
       </div>
       <Divider mx="xs" my="xl" />
-      <About /> 
+      <About />
       <Divider mx="xs" my="xl" />
       <Skills />
       <Divider mx="xs" my="xl" />
-      <Experience /> 
+      <Experience />
       <Divider mx="xs" my="xl" />
       <Certificate />
     </div>
