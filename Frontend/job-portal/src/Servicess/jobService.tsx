9f0be68b9f6c5367d74c234c01d85ea46fd05ffd
soumen kumar bera <soumenbera4017@gmail.com>
2026-08-01 16:35:35 +0530

    api add

--- Frontend/job-portal/src/Servicess/jobService.tsx
+++ Frontend/job-portal/src/Servicess/jobService.tsx
@@ -1,39 +1,58 @@
 import axios from "axios";
-const base_url = "http://localhost:8080/job/"
+const base_url = "http://localhost:8080/job/";
 
+const postJob = async (job: any) => {
+  return axios
+    .post(`${base_url}post`, job)
+    .then((result) => result.data)
+    .catch((error) => {
+      throw error;
+    });
+};
 
-const postJob = async (job:any) =>{
+const getAllJobs = async () => {
+  return axios
+    .get(`${base_url}getAll`)
+    .then((result) => result.data)
+    .catch((error) => {
+      throw error;
+    });
+};
 
-   return axios.post(`${base_url}post`, job)
-   .then(result => result.data)
-   .catch(error => { throw error});
+const getJob = async (id: any) => {
+  return axios
+    .get(`${base_url}get/${id}`)
+    .then((result) => result.data)
+    .catch((error) => {
+      throw error;
+    });
+};
 
-}
+const ApplyJob = async (id: any, applicant: any) => {
+  return axios
+    .post(`${base_url}apply/${id}`, applicant)
+    .then((result) => result.data)
+    .catch((error) => {
+      throw error;
+    });
+};
 
-const getAllJobs = async () =>{
+const getJobPostedBy = async (id: any) => {
+  return axios
+    .get(`${base_url}postedBy/${id}`)
+    .then((result) => result.data)
+    .catch((error) => {
+      throw error;
+    });
+};
 
-   return axios.get(`${base_url}getAll`)
-   .then(result => result.data)
-   .catch(error => { throw error});
+const changeAppStatus = async (application: any) => {
+  return axios
+    .post("${base_url}changeAppStatus", application)
+    .then((result) => result.data)
+    .catch((error) => {
+      throw error;
+    });
+};
 
-}
-
-const getJob = async (id:any) =>{
-
-   return axios.get(`${base_url}get/${id}`)
-   .then(result => result.data)
-   .catch(error => { throw error});
-
-}
-
-const ApplyJob = async (id:any, applicant:any) =>{
-
-   return axios.post(`${base_url}apply/${id}`, applicant)
-   .then(result => result.data)
-   .catch(error => { throw error});
-
-}
-
-
-
-export {postJob, getAllJobs, getJob, ApplyJob};
+export { postJob, getAllJobs, getJob, ApplyJob, getJobPostedBy, changeAppStatus };
