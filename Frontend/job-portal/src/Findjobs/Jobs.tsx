5ff9dfbc8d0d0ff3d263ccc13aa8ae8600219395
soumen kumar bera <soumenbera4017@gmail.com>
2026-08-12 15:08:30 +0530

    findJob daynamik ready

--- Frontend/job-portal/src/Findjobs/Jobs.tsx
+++ Frontend/job-portal/src/Findjobs/Jobs.tsx
@@ -3,23 +3,78 @@ import Sort from "./Sort";
 // import { jobList } from "../Data/JobsData";
 import { useEffect, useState } from "react";
 import { getAllJobs } from "../Servicess/jobService";
+import { useSelector } from "react-redux";
 
 const Jobs = () => {
+  const stateFilter = useSelector((state: any) => state.filter);
+  const [jobListFilter, setJobListFilter] = useState<any[]>([]);
 
-   const [jobList, seetJobList] = useState([{}]);
+  const [jobList, seetJobList] = useState([{}]);
 
-   useEffect (() =>{
-
+  useEffect(() => {
     getAllJobs()
-    .then(res =>{
-      seetJobList(res.filter((job:any) => job.jobStatus == 'ACTIVE'));
-    }).catch( error => {
+      .then((res) => {
+        seetJobList(res.filter((job: any) => job.jobStatus == "ACTIVE"));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-      console.log(error);
+  useEffect(() => {
+    let filterJob = jobList;
 
-    })
+    if (stateFilter["Job Title"] && stateFilter["Job Title"].length > 0) {
+      filterJob = filterJob.filter((role: any) =>
+        (stateFilter["Job Title"] ?? []).some((title: any) =>
+          (role.jobTitle ?? "")
+            .toLowerCase()
+            .includes(String(title ?? "").toLowerCase()),
+        ),
+      );
+    }
+    if (stateFilter["Location"] && stateFilter["Location"].length > 0) {
+      filterJob = filterJob.filter((role: any) =>
+        (stateFilter["Location"] ?? []).some((title: any) =>
+          (role.location ?? "")
+            .toLowerCase()
+            .includes(String(title ?? "").toLowerCase()),
+        ),
+      );
+    }
+    if (stateFilter["Experience"] && stateFilter["Experience"].length > 0) {
+      filterJob = filterJob.filter((role: any) =>
+        (stateFilter["Experience"] ?? []).some((title: any) =>
+          (role.experience ?? "")
+            .toLowerCase()
+            .includes(String(title ?? "").toLowerCase()),
+        ),
+      );
+    }
 
-   }, []);
+    if (stateFilter["Job Type"] && stateFilter["Job Type"].length > 0) {
+      filterJob = filterJob.filter((role: any) =>
+        (stateFilter["Job Type"] ?? []).some((title: any) =>
+          (role.jobType ?? "")
+            .toLowerCase()
+            .includes(String(title ?? "").toLowerCase()),
+        ),
+      );
+    }
+
+    if (stateFilter.package?.length === 2) {
+      filterJob = filterJob.filter((talent: any) => {
+        const experience = Number(talent.packageOffered ?? 0);
+
+        return (
+          experience >= stateFilter.package[0] &&
+          experience <= stateFilter.package[1]
+        );
+      });
+    }
+
+    setJobListFilter(filterJob);
+  }, [stateFilter, jobList]);
 
   return (
     <div className="p-5 mt-7">
@@ -30,7 +85,7 @@ const Jobs = () => {
         </div>
       </div>
       <div className="mt-10 gap-8 flex flex-wrap justify-center">
-        {jobList.map((job, index) => (
+        {jobListFilter?.map((job, index) => (
           <JobCart key={index} {...job} />
         ))}
       </div>
