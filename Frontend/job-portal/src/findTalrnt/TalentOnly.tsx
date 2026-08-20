551da984a62ee1097e3cf85a76ca27b88c757916
soumen kumar bera <soumenbera4017@gmail.com>
2026-08-20 16:59:12 +0530

    respon..... complete  findTalentPage

--- Frontend/job-portal/src/findTalrnt/TalentOnly.tsx
+++ Frontend/job-portal/src/findTalrnt/TalentOnly.tsx
@@ -25,7 +25,7 @@ const TalentOnly = (props: any) => {
 
   
   return (
-    <div className="bg-mine-shaft-700 p-4 w-96 flex flex-col gap-3 rounded-xl hover:shadow-[0_0_5px_1px_yellow] !bright-sun-400 cursor-pointer mb-5">
+    <div className="bg-mine-shaft-700 p-4 w-96 flex  flex-col gap-3 rounded-xl hover:shadow-[0_0_5px_1px_yellow] !bright-sun-400 cursor-pointer mb-5 bs-mx:w-full">
       <div className="flex justify-between ">
         <div className="flex gap-2 items-center">
           <div className="p-2 bg-mine-shaft-600 rounded-full">
@@ -47,7 +47,7 @@ const TalentOnly = (props: any) => {
         </div>
         <IconHeart stroke={2} className="text-mine-shaft-300 cursor-pointer" />
       </div>
-      <div className="flex gap-2 [&>div]:px-2 [&>div]:py-1 [&>div]:bg-mine-shaft-600 [&>div]:rounded-lg [&>div]:text-xs [&>div]:text-bright-sun-400 mt-4">
+      <div className="flex flex-wrap gap-2 [&>div]:px-2 [&>div]:py-1 [&>div]:bg-mine-shaft-600 [&>div]:rounded-lg [&>div]:text-xs [&>div]:text-bright-sun-400 mt-4">
         {props.skills?.map(
           (skill: any, index: any) =>
             index < 3 && <div key={index}>{skill}</div>,
